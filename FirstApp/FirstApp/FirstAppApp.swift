901fcff467012edc73b944e85ed8c71a6331cd5d
Evan Bee <evan2884@gmail.com>
2024-02-10 22:38:13 +0800

    FistApp

---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by MyZiyue on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
