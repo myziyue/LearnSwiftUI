2bb705bf63d5fb51ce3cc5b85cc9ed43b318c9df
Evan Bee <evan2884@gmail.com>
2024-02-11 21:07:34 +0800

    Change FirstApp files

--- FirstApp/FirstApp/CardView.swift
+++ FirstApp/FirstApp/CardView.swift
@@ -14,7 +14,21 @@ struct CardView: View {
     // Mark: - Body
     var body: some View {
         ZStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image("developer-no1")
+            
+            VStack{
+                Text("SwiftUI")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                Text("如此美妙 不同凡响")
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .italic()
+            }//:VStack
+            .offset(y: -218)
         } //: ZStack
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .top, endPoint: .bottom))
