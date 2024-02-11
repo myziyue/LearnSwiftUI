570ccca249de4fe2e87e98e68fe4fc0921f76fb0
Evan Bee <evan2884@gmail.com>
2024-02-11 18:15:21 +0800

    Change FirstApp files

---
+++ FirstApp/FirstApp/CardView.swift
@@ -0,0 +1,22 @@
+//
+//  CardView.swift
+//  FirstApp
+//
+//  Created by MyZiyue on 2024-02-11.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    // Mark: - Properties
+    
+    // Mark: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// Mark: - Preview
+#Preview {
+    CardView()
+}
